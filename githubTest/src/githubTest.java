275fad5e707ea56cea430077a3984e455c2d3dd4
jakersmith <71725421+jakersmith@users.noreply.github.com>
2022-07-07 12:48:18 -0600

    Update githubTest.java

--- githubTest/src/githubTest.java
+++ githubTest/src/githubTest.java
@@ -2,5 +2,6 @@ public class githubTest {
 
     public static void main(String[] args) {
         System.out.println("This is a test of github. I am learning!");
+        System.out.println("Here is another string");
     }
 }
