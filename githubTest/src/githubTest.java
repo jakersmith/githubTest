f824e01867ff46ab58b86f46e364f7fa05df135b
jakersmith <71725421+jakersmith@users.noreply.github.com>
2022-07-07 13:21:37 -0600

    Added print100Times method
    Added this method to allow users to create an input, then print it an unnecessary number of times.

--- githubTest/src/githubTest.java
+++ githubTest/src/githubTest.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class githubTest {
 
     public static void main(String[] args) {
         System.out.println("This is a test of github. I am learning!");
         System.out.println("Here is another string");
+
+        Scanner scanner = new Scanner(System.in);
+        print100Times(scanner.next());
     }
+
+
+
+    private static void print100Times (String input) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(input);
+        }
+    }
+
 }
